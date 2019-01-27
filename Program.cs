811b827de5a65cb0218b00fc7b53b5387c64f1c9
Alex <quacksterr.15@gmail.com>
2019-01-28 05:02:36 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Squirtle
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            log.Info("Booting prjSquirtle - Written by Quackster");
+            log.Info("Habbo Hotel 2001 emulation of V1");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Util/Logging/ConsoleAppenderWithColorSwitching.cs
@@ -0,0 +1,41 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Squirtle.Util.Logging
+{
+    public class ConsoleAppenderWithColorSwitching : ConsoleAppender
+    {
+        private string ReplaceLogLevel(string renderedLayout)
+        {
+            renderedLayout = renderedLayout.Replace(" INFO ", " |DarkGreen|INFO|Gray| ");
+            renderedLayout = renderedLayout.Replace(" DEBUG ", " |Blue|DEBUG|Gray|  ");
+            renderedLayout = renderedLayout.Replace(" WARN ", " |DarkYellow|DEBUG|Gray|  ");
+            renderedLayout = renderedLayout.Replace(" ERROR ", " |DarkRed|ERROR|Gray|  ");
+            return renderedLayout;
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            var regex = new Regex(@"(\|\w+\|)");
+            var renderedLayout = base.RenderLoggingEvent(loggingEvent);
+            renderedLayout = ReplaceLogLevel(renderedLayout);
+
+            var chunks = regex.Split(renderedLayout);
+
+            foreach (var chunk in chunks)
+            {
+                if (chunk.StartsWith("|") && chunk.EndsWith("|"))
+                {
+                    var consoleColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), chunk.Substring(1, chunk.Length - 2));
+                    Console.ForegroundColor = consoleColor;
+                }
+                else
+                {
+                    Console.Write(chunk);
+                }
+            }
+        }
+    }
+}
