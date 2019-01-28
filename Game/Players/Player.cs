532de55f6677f02e73537f4f06e580723af22b40
Alex <quacksterr.15@gmail.com>
2019-01-28 15:42:09 +1000

    Add response class

--- Game/Players/Player.cs
+++ Game/Players/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetty.Transport.Channels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,32 @@ namespace Squirtle.Game.Players
 {
     class Player
     {
+        private IChannel _channel;
+
+        /// <summary>
+        /// Gets the player channel
+        /// </summary>
+        public IChannel Channel
+        {
+            get { return _channel; }
+        }
+
+        /// <summary>
+        /// Constructor for player
+        /// </summary>
+        /// <param name="channel">the channel</param>
+        public Player(IChannel channel)
+        {
+            _channel = channel;
+        }
+
+        /// <summary>
+        /// Send an object to the player's channel
+        /// </summary>
+        /// <param name="obj">the object to send</param>
+        public void Send(object obj)
+        {
+            _channel.WriteAndFlushAsync(obj);
+        }
     }
 }
---
+++ Network/Streams/Response.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Squirtle.Network.Streams
+{
+    class Response
+    {
+        private string _header;
+        private bool _finalised;
+
+        private StringBuilder _buffer;
+
+        /// <summary>
+        /// Get the message header
+        /// </summary>
+        public string Header
+        {
+            get { return _header; }
+        }
+        
+        /// <summary>
+        /// Get the message buffer
+        /// </summary>
+        public StringBuilder Buffer
+        {
+            get { return _buffer; }
+        }
+
+        /// <summary>
+        /// Get the message body
+        /// </summary>
+        public string Body
+        {
+            get { return _buffer.ToString(); }
+        }
+
+        /// <summary>
+        /// Constructor for request class.
+        /// </summary>
+        /// <param name="messageHeader">the header requested</param>
+        /// <param name="messageData">the data sent from client</param>
+        public Response(string Header)
+        {
+            _header = Header;
+
+            _buffer = new StringBuilder();
+            _buffer.Append("#");
+            _buffer.Append(_header);
+        }
+
+        /// <summary>
+        /// Append a raw object to the response
+        /// </summary>
+        /// <param name="data">the data to send</param>
+        public void Append(object data)
+        {
+            string value = data.ToString();
+            value = value.Replace("#", "*");
+
+            _buffer.Append(value);
+        }
+
+        /// <summary>
+        /// Get the finalised message
+        /// </summary>
+        /// <returns>the created message</returns>
+        public string GetMessage()
+        {
+            if (!_finalised)
+            {
+                _finalised = true;
+                _buffer.Append("#");
+                _buffer.Append("#");
+            }
+
+            return _buffer.ToString();
+        }
+    }
+}
