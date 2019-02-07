5236be5a66ab3b63ce9ce94ea065941ae8f27f72
Alex <quacksterr.15@gmail.com>
2019-02-07 21:43:14 +1000

    Include drink animation in DROPDRINK

--- Messages/Incoming/DROPDRINK.cs
+++ Messages/Incoming/DROPDRINK.cs
@@ -8,10 +8,11 @@ namespace Squirtle.Messages
     {
         public void Handle(Player player, Request request)
         {
-            if (!player.RoomUser.Status.ContainsKey("carryd"))
+            if (!player.RoomUser.Status.ContainsKey("carryd") && !player.RoomUser.Status.ContainsKey("drink"))
                 return;
 
             player.RoomUser.RemoveStatus("carryd");
+            player.RoomUser.RemoveStatus("drink");
             player.RoomUser.NeedsUpdate = true;
         }
     }
